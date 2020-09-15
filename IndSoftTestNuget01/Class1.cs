141faabbd4c0a8a1abd7586d0f5b7bf233c3d01a
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-15 08:24:07 +0200

    TestNuget, zalozeni

---
+++ IndSoftTestNuget01/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndSoftTestNuget01
+{
+
+  public class CheckMFAResponse
+  {
+    public string AuthorizationStatus { get; set; }
+    public string CustomId { get; set; }
+  }
+
+  public class NugetMain
+  {
+
+    public static void TestJSON()
+    { 
+      
+    }
+
+
+    public CheckMFAResponse GetResponse()
+    {
+      string sFileContent = "{\"AuthorizationStatus\":\"Invalid\",\"CustomId\":\"\"}";
+
+      //var serializer = new Newtonsoft.Json.Serialization.j
+      var test = Newtonsoft.Json.JsonConvert.DeserializeObject<CheckMFAResponse>(
+                 sFileContent,
+                 new Newtonsoft.Json.JsonSerializerSettings
+                 {
+                   ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
+                   Formatting = Newtonsoft.Json.Formatting.Indented
+                 });
+
+      return test;
+    }
+  }
+}
