183903e63902690c14c3cb5d6d7b4518ecd18155
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-16 09:07:28 +0200

    Test publish nupkg

--- IndSoftTestNuget01/Class1.cs
+++ IndSoftTestNuget01/Class1.cs
@@ -24,7 +24,7 @@ namespace IndSoftTestNuget01
 
     public CheckMFAResponse GetResponse()
     {
-      string sFileContent = "{\"AuthorizationStatus\":\"Invalid123abc\",\"CustomId\":\"\"}";
+      string sFileContent = "{\"AuthorizationStatus\":\"Invalid123abc456\",\"CustomId\":\"\"}";
 
       //var serializer = new Newtonsoft.Json.Serialization.j
       var test = Newtonsoft.Json.JsonConvert.DeserializeObject<CheckMFAResponse>(
