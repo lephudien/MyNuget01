dcf61ee1cbde6fd3adf228cd40b8255ca6ebcf59
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-18 10:39:00 +0200

    V37 + new DLL

---
+++ TestNuget01b/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNuget01b
+{
+  public class Class1Nuget01b
+  {
+    public static string GetMyVersion()
+    {
+      return "MyNugget01bVer=1.3.5.37";
+    }
+  }
+}
